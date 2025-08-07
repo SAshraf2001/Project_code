c89efd597341818d684fae921625ac2c818ef9a5
Sharjeel <sa062385@gmail.com>
2025-08-07 10:04:17 +0500

    Final

--- ilist/views.py
+++ ilist/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render
 from django.contrib import messages
 
 
-from .models import Todo
+from .models import Todo, Status
 # Create your views here.
 def content(request):
     if request.method == "POST":
         title = request.POST['title']
         content = request.POST['content']
-        status = request.POST['status']
-
-        my_cont = Todo(title=title, content=content, status_id=status)
+        statusId = request.POST['status']
+        
+        status_obj = Status.objects.get(statusName=statusId)
+        my_cont = Todo(title=title, content=content, status=status_obj)
         my_cont.save()
         messages.success(request, 'Your task has been submitted successfully:')
-    return render(request, 'todo/todohome.html')
+    statuses = Status.objects.all()
+    return render(request, 'todo/todohome.html', {'statuses':statuses})
 
 def displayTodo(request):
     alllist = Todo.objects.all()
@@ -21,3 +23,4 @@ def displayTodo(request):
         'allList':alllist
     }
     return render(request, 'todo/viewAll.html', context)
+
