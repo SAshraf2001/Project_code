99801c731fd472435d87a9e3b8d7ce7df1210e8a
Sharjeel <sa062385@gmail.com>
2025-12-29 18:56:40 +0500

    Final Comment

--- db_sem_project/settings.py
+++ db_sem_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import pymysql
+pymysql.version_info = (2, 2, 7, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
